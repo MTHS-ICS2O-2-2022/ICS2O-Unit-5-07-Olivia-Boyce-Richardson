20209548fc06f96c3109319971a062fef7149d00
Olivia-Boyce-Richardson <124373077+Olivia-Boyce-Richardson@users.noreply.github.com>
2023-05-23 15:43:49 +0000

    changes

--- js/script.js
+++ js/script.js
@@ -1,5 +1,18 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2020 Mr Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: dominik
+// Created on: May 2023
 // This file contains the JS functions for index.html
+
+"use strict"
+
+function calculate() {
+  const integer = parseFloat(document.getElementById("highestInteger").value)
+  let output = 0
+
+  for (let counter = 1; counter <= integer; counter++) {
+    output += counter
+  }
+
+  document.getElementById("output").innerHTML = "The sum is: " + output
+}
